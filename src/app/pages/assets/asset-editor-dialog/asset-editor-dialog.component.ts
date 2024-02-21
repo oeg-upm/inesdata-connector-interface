a12e7f99d8cbf18f0277e24c1980c259d574bfc0
ppel <ppel@gmv.com>
2024-02-21 17:46:57 +0100

    #422753 - Added HttpData and AmazonS3 fields

--- src/app/pages/assets/asset-editor-dialog/asset-editor-dialog.component.ts
+++ src/app/pages/assets/asset-editor-dialog/asset-editor-dialog.component.ts
@@ -1,8 +1,10 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { AssetInput, DataAddress } from "@think-it-labs/edc-connector-client";
+import { Component, Inject } from '@angular/core';
+import { AssetInput, HttpDataAddress, DataAddress } from '@think-it-labs/edc-connector-client';
 import { MatDialogRef } from "@angular/material/dialog";
 import { StorageType } from "../../../shared/models/storage-type";
+import { AmazonS3DataAddress } from "../../../shared/models/inesdata-data-address";
 import { NotificationService } from 'src/app/shared/services/notification.service';
+import { DATA_ADDRESS_TYPES } from 'src/app/shared/utils/app.constants';
 
 
 @Component({
@@ -20,13 +22,26 @@ export class AssetEditorDialog {
 
   // AmazonS3 dataAddress attributes
   region: string = '';
-  keyName: string = '';
-  bucketName: string = '';
+  bucketName?: string;
+  keyPrefix?: string;
+  folderName?: string;
+  accessKeyId?: string;
+  secretAccessKey?: string;
+  endpointOverride?: string;
 
   // HttpData dataAddress attributes
-  httpDataName: string = '';
-  baseUrl: string = '';
-  proxyPath: string = '';
+  httpDataName?: string;
+  path?: string;
+  method?: string;
+  baseUrl?: string;
+  authKey?: string;
+  authCode?: string;
+  secretName?: string;
+  proxyBody?: string;
+  proxyPath?: string;
+  proxyQueryParams?: string;
+  proxyMethod?: string;
+  contentType?: string;
 
 
   constructor(private dialogRef: MatDialogRef<AssetEditorDialog>,
@@ -40,22 +55,37 @@ export class AssetEditorDialog {
       return;
     }
 
-    let dataAddress: any;
+    let dataAddress: DataAddress;
 
-    if (this.storageTypeId === 'AmazonS3') {
-      dataAddress = {
-        "type": this.storageTypeId,
-        "region": this.region,
-        "keyName": this.keyName,
-        "bucketName": this.bucketName
+    if (this.storageTypeId === DATA_ADDRESS_TYPES.amazonS3) {
+      const amazonS3DataAddress: AmazonS3DataAddress = {
+        type: 'AmazonS3',
+        region: this.region,
+        bucketName: this.bucketName,
+        keyPrefix: this.keyPrefix,
+        folderName: this.folderName,
+        accessKeyId: this.accessKeyId,
+        secretAccessKey: this.secretAccessKey,
+        endpointOverride: this.endpointOverride
       };
-    } else if (this.storageTypeId === 'HttpData') {
-      dataAddress = {
-        "type": this.storageTypeId,
-        "name": this.httpDataName,
-        "baseUrl": this.baseUrl,
-        "proxyPath": this.proxyPath
+      dataAddress = amazonS3DataAddress;
+    } else if (this.storageTypeId === DATA_ADDRESS_TYPES.httpData) {
+      const httpDataAddress: HttpDataAddress = {
+        type: 'HttpData',
+        name: this.httpDataName,
+        path: this.path,
+        method: this.method,
+        baseUrl: this.baseUrl,
+        authKey: this.authKey,
+        authCode: this.authCode,
+        secretName: this.secretName,
+        proxyBody: this.proxyBody,
+        proxyPath: this.proxyPath,
+        proxyQueryParams: this.proxyQueryParams,
+        proxyMethod: this.proxyMethod,
+        contentType: this.contentType,
       };
+      dataAddress = httpDataAddress;
     } else {
       this.notificationService.showError("Incorrect destination value");
       return;
@@ -74,11 +104,20 @@ export class AssetEditorDialog {
     this.dialogRef.close({ assetInput });
   }
 
+  /**
+   * Checks the required fields
+   *
+   * @returns true if required fields have been filled
+   */
   private checkRequiredFields(): boolean {
-    if(!this.id || !this.storageTypeId){
+    if (!this.id || !this.storageTypeId){
       return false;
     } else {
-      return true;
+      if (this.storageTypeId === DATA_ADDRESS_TYPES.amazonS3 && !this.region) {
+        return false;
+      } else {
+        return true;
+      }
     }
   }
 }
