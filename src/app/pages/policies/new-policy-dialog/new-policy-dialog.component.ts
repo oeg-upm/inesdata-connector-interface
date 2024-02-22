4fd9883c2100e4773870132be9f6d1d686d938fc
ppel <ppel@gmv.com>
2024-02-22 18:10:32 +0100

    #422756 - Added JSON parser error

--- src/app/pages/policies/new-policy-dialog/new-policy-dialog.component.ts
+++ src/app/pages/policies/new-policy-dialog/new-policy-dialog.component.ts
@@ -1,6 +1,7 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {PolicyDefinitionInput, PolicyInput} from "../../../shared/models/edc-connector-entities";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import { Component, OnInit } from '@angular/core';
+import { PolicyDefinitionInput, PolicyInput } from "../../../shared/models/edc-connector-entities";
+import { MatDialogRef } from "@angular/material/dialog";
+import { NotificationService } from 'src/app/shared/services/notification.service';
 
 @Component({
   selector: 'app-new-policy-dialog',
@@ -20,7 +21,8 @@ export class NewPolicyDialogComponent implements OnInit {
   prohibitionsJson: string = '';
   obligationsJson: string = '';
 
-  constructor(private dialogRef: MatDialogRef<NewPolicyDialogComponent>) {
+  constructor(private dialogRef: MatDialogRef<NewPolicyDialogComponent>,
+              private notificationService: NotificationService,) {
   }
 
   ngOnInit(): void {
@@ -28,24 +30,31 @@ export class NewPolicyDialogComponent implements OnInit {
   }
 
   onSave() {
-    if (this.permissionsJson && this.permissionsJson !== '') {
-      this.policy.permission = JSON.parse(this.permissionsJson);
+    try {
+      if (this.permissionsJson && this.permissionsJson !== '') {
+        this.policy.permission = JSON.parse(this.permissionsJson);
+      }
+
+      if (this.prohibitionsJson && this.prohibitionsJson !== '') {
+        this.policy.prohibition = JSON.parse(this.prohibitionsJson);
+      }
+
+      if (this.obligationsJson && this.obligationsJson !== '') {
+        this.policy.obligation = JSON.parse(this.obligationsJson);
+      }
+    } catch (error) {
+      if (error instanceof SyntaxError) {
+        this.notificationService.showError("Error parsing JSON: " + error.message);
+        console.error(error);
+        return;
+      }
     }
 
-    if (this.prohibitionsJson && this.prohibitionsJson !== '') {
-      this.policy.prohibition = JSON.parse(this.prohibitionsJson);
-    }
-
-    if (this.obligationsJson && this.obligationsJson !== '') {
-      this.policy.obligation = JSON.parse(this.obligationsJson);
-    }
-
-    this.policy["@context"]="http://www.w3.org/ns/odrl.jsonld"
+    this.policy["@context"] = "http://www.w3.org/ns/odrl.jsonld"
 
 
     this.dialogRef.close({
-
-      policy : this.policyDefinition.policy,
+      policy: this.policyDefinition.policy,
       '@id': this.policyDefinition.id
     })
   }
